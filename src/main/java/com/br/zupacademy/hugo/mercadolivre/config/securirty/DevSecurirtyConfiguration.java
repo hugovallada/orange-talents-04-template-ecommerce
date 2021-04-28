9aaaffa7359faa80adda367610cabfb949e32459
hugovallada <valladahugo@gmail.com>
2021-04-28 13:22:05 -0300

    Adiciona o Spring Securirty e Cria a classe de configuraçãodo Spring Security

---
+++ src/main/java/com/br/zupacademy/hugo/mercadolivre/config/securirty/DevSecurirtyConfiguration.java
@@ -0,0 +1,37 @@
+package com.br.zupacademy.hugo.mercadolivre.config.securirty;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Essa classe será usada apenas em ambiente de desenvolvimento, por isso o acesso a todas as rotas estará liberado.
+ */
+@EnableWebSecurity
+@Configuration
+public class DevSecurirtyConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .csrf().disable();
+    }
+}
